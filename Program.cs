639fd4a10fa45c7ba1383bfd102bb3c6f0031f45
Xavaltir <tombis1999@hotmail.com>
2019-09-25 10:19:52 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gameboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfRows = 8;
+            int numberOfColumns = 9;
+
+            int cellNumber = 8;
+
+
+           Console.WriteLine("Number of columns is " + numberOfColumns);
+           int cellRownumber = (cellNumber - 1) / numberOfColumns + 1;
+           int cellColumnNumber = (cellNumber - 1) % numberOfColumns + 1;
+
+           Console.WriteLine("Cell" + cellNumber + " is at row" + cellRownumber + " and column " + cellColumnNumber);
+
+            cellNumber = 16;
+            cellRownumber = (cellNumber - 1) / numberOfColumns + 1;
+            cellColumnNumber = (cellNumber - 1) % numberOfColumns + 1;
+            
+            Console.WriteLine("Cell" + cellNumber + " is at row" + cellRownumber + " and column " + cellColumnNumber);
+
+            cellNumber = 17;
+            cellRownumber = (cellNumber - 1) / numberOfColumns + 1;
+            cellColumnNumber = (cellNumber - 1) % numberOfColumns + 1;
+
+            Console.WriteLine("Cell" + cellNumber + " is at row" + cellRownumber + " and column " + cellColumnNumber);
+
+            cellNumber = 23;
+            cellRownumber = (cellNumber - 1) / numberOfColumns + 1;
+            cellColumnNumber = (cellNumber - 1) % numberOfColumns + 1;
+
+            Console.WriteLine("Cell" + cellNumber + " is at row" + cellRownumber + " and column " + cellColumnNumber);
+            Console.ReadLine();
+
+
+        }
+    }
+}
